7817f42a3cd9096128846f51289a9b896f4b880c
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-03 12:53:54 -0500

    implemented the logic that takes a users input and adds it to the task array

--- src/App.js
+++ src/App.js
@@ -5,11 +5,48 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      task: { text: ''},
+      tasks: [],
+    };
+
+    
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+      }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '' },
+    })
+    
   }
   
   render() {
+
+    const { task } = this.state;
+
     return (
       <div className="App">
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter A Task: </label>
+          <input 
+            onChange={this.handleChange}
+            value={task.text}
+            type='text'
+            id='taskInput'
+          />
+          <button type='submit'>Add Task</button>
+        </form>
         <Overview />
       </div>
     );
