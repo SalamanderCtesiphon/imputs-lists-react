61baa6e835fc1889dddd14c2f236c9c9bb9195f6
salamanderctesiphon <swbrookshire@gmail.com>
2023-04-03 11:56:22 -0500

    added the files for the assignment and refactored app to a class based component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
+import React, { Component } from 'react';
+import Overview from './components/Overview';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Overview />
+      </div>
+    );
+  }
 }
 
 export default App;
